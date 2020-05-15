e278787d284294777381010f8264a6685c16475f
Matthew Kuyath <matthewkuyath@Matthews-MacBook-Air.local>
2020-05-14 22:12:12 -0500

    rewrote App.js to fetch data from Vidscrip API, randomily sort in an array, and display.

--- src/App.js
+++ src/App.js
@@ -1,27 +1,67 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Well done, Matt! Now you can see the power of git/github.com
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function shuffleArray(array) {
+    for (var i = array.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
 }
 
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      items: []
+    };
+  }
+
+  componentDidMount() {
+    fetch('https://api.vidscrip.com/api/v1/specialties')
+      .then(res => res.json())
+      .then(
+        (result) => {
+          const specialties = result.data.specialties
+          this.setState({
+            isLoaded: true,
+            items: specialties
+          });
+        },
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      )
+  }
+
+  render() {
+    const { error, isLoaded, items } = this.state;
+    if (error) {
+      return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+      return <div>Loading...</div>;
+    } else {
+        shuffleArray(items);
+        shuffleArray(items);
+      return (
+        <ul>
+          {items.map(item => 
+            <li key={item.name}>
+                {item.name}
+            </li>
+          )}
+        </ul>
+      );
+    }
+  }
+}
 
 export default App;
+
+
